bc0ccfc9bf01c65de73c71094b6d86ea4c6b634d
Vitali Sharametsyeu <vitali.sharametsyeu@cbitech.ae>
2025-09-29 10:32:48 +0300

    update

--- fix-jitsi-p2p.sh
+++ fix-jitsi-p2p.sh
@@ -263,8 +263,67 @@ CONF
         echo "📋 Содержимое строк 25-35:"
         docker compose exec prosody bash -lc "cat /etc/prosody/prosody.cfg.lua | sed -n '25,35p'"
         echo "🔧 Исправляю конфигурацию..."
-        # Исправляем лишнюю запятую в modules_enabled
-        docker compose exec prosody bash -lc "sed -i 's/\"mam\";/\"mam\"/' /etc/prosody/prosody.cfg.lua"
+        # Полностью перезаписываем конфигурацию Prosody
+        docker compose exec prosody bash -lc "cat > /etc/prosody/prosody.cfg.lua << 'EOF'
+admins = {}
+
+modules_enabled = {
+  \"websocket\";
+  \"bosh\";
+  \"ping\";
+  \"roster\";
+  \"pep\";
+  \"private\";
+  \"blocklist\";
+  \"vcard4\";
+  \"vcard_legacy\";
+  \"limits\";
+  \"csi_simple\";
+  \"mam\";
+  \"http\";
+}
+
+modules_disabled = {
+  \"posix\";
+}
+
+http_ports = { 5280 }
+https_ports = { }
+http_interfaces = { \"*\" }
+
+-- Явное сопоставление путей модулей
+http_paths = {
+  bosh = \"/http-bind\";
+  websocket = \"/xmpp-websocket\"
+}
+
+cross_domain_websocket = true
+consider_bosh_secure = true
+
+-- Отключаем требование TLS глобально
+c2s_require_encryption = false
+s2s_require_encryption = false
+
+VirtualHost \"connect.mooz.pro\"
+  authentication = \"anonymous\"
+  allow_unencrypted_plain_auth = true
+  ssl = { }
+  modules_enabled = {
+    \"websocket\"; \"bosh\"; \"ping\"; \"mam\"
+  }
+  -- Отключаем требование TLS для внутренних соединений
+  c2s_require_encryption = false
+  s2s_require_encryption = false
+
+VirtualHost \"auth.connect.mooz.pro\"
+  authentication = \"internal_hashed\"
+
+Component \"conference.connect.mooz.pro\" \"muc\"
+  restrict_room_creation = false
+  max_history_messages = 0
+  muc_room_locking = false
+  muc_tombstones = false
+EOF"
         docker compose restart prosody
         sleep 3
     else
